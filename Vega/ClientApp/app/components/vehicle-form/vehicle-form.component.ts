070beef7c7f577d08b65540627152b16a25fec3d
pazdernikpavel <33874597+pazdernikpavel@users.noreply.github.com>
2018-01-05 18:06:06 +0100

    Front-end: adding directives

--- Vega/ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ Vega/ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -12,7 +12,9 @@ export class VehicleFormComponent implements OnInit {
   makes: any[];
   models: any[];
   features: any[];
-  vehicle: any = {};
+  vehicle: any = {
+    features: []
+  };
 
   constructor(
     private vehicleService: VehicleService) { }
@@ -30,9 +32,18 @@ export class VehicleFormComponent implements OnInit {
   }
 
   onMakeChange() {
-    var selectedMake = this.makes.find(m => m.id == this.vehicle.make)
+    var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId)
     this.models = selectedMake ? selectedMake.models : [];
-    console.log("changed", selectedMake.models);
+    delete this.vehicle.modelId;
+  }
+
+  onFeatureToggle(featureId: any, $event: any) {
+    if ($event.target.checked)
+      this.vehicle.features.push(featureId);
+    else {
+      var index = this.vehicle.features.indexOf(featureId);
+      this.vehicle.features.splice(index, 1);
+    } 
   }
 
 }
